a094a1a5cfc525f6299bb33d4b373827ddc068de
Ha Truong <truonghh@beloit.edu>
2020-08-27 23:11:26 -0700

    Add script to support 3d objects' jumping effect

---
+++ scripts/jumping.js
@@ -0,0 +1,24 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+export const Diagnostics = require('Diagnostics');
+const Scene = require('Scene');
+const Animation = require('Animation');
+const pokemonList = ['Pikachu3d', 'Charmander3d', 'Bulbasaur3d'];
+
+for (var pokemon of pokemonList) {
+	Scene.root.findFirst(pokemon).then(jump);
+}
+
+function jump(object) {
+	var baseDriverParameters = {
+	  durationMilliseconds: 400,
+	  loopCount: Infinity,
+	  mirror: true
+	};
+	var driver = Animation.timeDriver(baseDriverParameters);
+	var sampler = Animation.samplers.linear(0, 0.05);
+	object.transform.y = Animation.animate(driver, sampler);
+	driver.start();
+}
